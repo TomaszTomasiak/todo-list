c563a00848637573d51be3eb1bd01acd584c2842
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2021-03-01 12:48:48 +0100

    $event - keyUp - KeyBoardEvent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,4 +39,9 @@ export class AppComponent {
   clearTasks() {
     this.tasks = [];
   }
+
+  onKeyUp(event: KeyboardEvent) {
+    const target = event.target as HTMLInputElement;
+    console.log(target.value);
+  }
 }
